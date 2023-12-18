2f5e37ad45b187a7b66dbedeba04d0c54b863699
saifali77 <saif_ali2017@hotmail.com>
2023-12-18 21:27:40 +0500

    Client - Angular setup

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'client';
+
+  constructor() {
+    setTheme('bs5');
+  }
+}
